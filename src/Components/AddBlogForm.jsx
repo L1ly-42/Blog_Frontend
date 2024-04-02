d0c5707479e834983cc5d6616e57296981ddc6e7
Leila Peltier <l_peltier@outlook.com>
2024-04-02 15:39:49 +0100

    Added ability to post a new blog

--- src/Components/AddBlogForm.jsx
+++ src/Components/AddBlogForm.jsx
@@ -3,6 +3,13 @@ import './AddBlogForm.css'
 
 const AddBlogForm = () => {
 const [blogs, setBlogs] = useState([]);
+const [name, setName] = useState("");
+const [blog, setBlog] = useState({
+    name : "Dont worry about it", 
+    dateOfCreation :"02/04/2014",
+    timeOfCreation : "16:00", 
+    userId : 3
+})
 
 const loadBlogs = async () => {
     const response = await fetch("http://localhost:8080/blogs");
@@ -11,8 +18,19 @@ const loadBlogs = async () => {
     setBlogs(jsonData);
 }
 
-const postBlogs = async () => {
-    const response = await fetch()
+const postBlogs = async (blog) => {
+    const response = await fetch("http://localhost:8080/blogs", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+    })
+    const savedBlog = await response.json();
+    setBlogs([...blogs,savedBlog]);
+    
+}
+
+const handleSubmit = () => {
+    postBlogs(blog);
 }
 
 useEffect(() =>{
@@ -21,7 +39,11 @@ useEffect(() =>{
 
     
     return (
-        <p>Blog creation form!</p>
+        <>
+            <p>Blog creation form!</p>
+            <button onClick={handleSubmit}>Add Blog</button>
+        </>
+
     );
 }
  

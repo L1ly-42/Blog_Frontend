b5300d804a9ba18f1cba7437078fd9fe59f69b8d
Sahil Patel <sahilpatel1906@gmail.com>
2024-04-02 06:51:54 -0500

    Created route for navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link, Outlet } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <>
+        <p> Navbar Goes Here! </p>
+        <Outlet />
+        </>
+    );
+}
+ 
+export default NavBar;
--- src/Containers/BlogContainer.jsx
+++ src/Containers/BlogContainer.jsx
@@ -1,6 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NavBar from "../Components/NavBar";
+
 const BlogContainer = () => {
+    const BlogRoutes = createBrowserRouter([
+        {
+            path: "/",
+            element: <></>,
+        },
+        {
+            path: "/1",
+            element: <NavBar />,
+        }
+    ])
+    
+    
     return ( 
-        <h1> Hello from BlogContainer! </h1>
+
+        <>
+        <h1> Hello from BlogContainer!</h1>
+        <RouterProvider router={BlogRoutes}/>
+        </>
+
     );
 }
  

332c8e7467c59b53a28d1c539ec28eaad9f20e1b
Leila Peltier <l_peltier@outlook.com>
2024-04-02 15:16:04 +0100

    testing addblogform fetch

--- src/Components/AddBlogForm.jsx
+++ src/Components/AddBlogForm.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import './AddBlogForm.css'
 
 const AddBlogForm = () => {
+const [blogs, setBlogs] = useState([]);
+
+const loadBlogs = async () => {
+    const response = await fetch("localhost:8080/blogs");
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
+
+useEffect(() =>{
+    loadBlogs();
+},[])
 
     
     return (

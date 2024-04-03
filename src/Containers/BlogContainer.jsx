b3855f1fcae85298ce58556b1ddf6f0ea412bf34
Leila Peltier <l_peltier@outlook.com>
2024-04-03 19:37:15 +0100

    Basic CSS done on footer

--- src/Containers/BlogContainer.jsx
+++ src/Containers/BlogContainer.jsx
@@ -128,28 +128,30 @@ const BlogContainer = () => {
             <header>
                 <h1> Blogtopia! </h1>
                 <img src={Logo} id="logo"/>
-
             </header>
+
             <main>
                 <RouterProvider router={BlogRoutes}/>
             </main>
             
             <footer>
-                <hr />
-                <h3>Contact Us:</h3>
-                <p>Email:</p>
-                <a href="zsolt@brightnetwork.co.uk">admin@blogtopia.com</a>
-                <p>Phone number: </p>
-                <a href="tel:0207 197 9902">+44 (5182) 797 9825</a>
+            <hr />
+                 <section>
+                    <div id="contact">
+                        <h3 id="contactTitle">Contact Us:</h3>
+                        <p>Email:</p>
+                        <a href="zsolt@brightnetwork.co.uk">admin@blogtopia.com</a>
+                        <p>Phone number: </p>
+                        <a href="tel:0207 197 9902">+44 (5182) 797 9825</a>
+                    </div>
 
-                <div id="copyright">
-                <p id="copyrightText"> &copy; Blogtopia 2024</p>
-                <img src={Logo} id="footerLogo"/>
+                    <div id="copyright">
+                        <p id="copyrightText"> &copy; Blogtopia 2024</p>
+                        <img src={Logo} id="footerLogo"/>
+                    </div>
 
-                </div>
-           
-
-            </footer>
+                </section>
+            </footer> 
         </>
 
     );

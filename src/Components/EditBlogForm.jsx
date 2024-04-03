0af25787017b8e2c57221c74f97dfbceee89b89b
Sahil Patel <sahilpatel1906@gmail.com>
2024-04-03 05:08:53 -0500

    created form for editing blog

--- src/Components/EditBlogForm.jsx
+++ src/Components/EditBlogForm.jsx
@@ -1,8 +1,43 @@
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import './EditBlogForm.css'
+import { useState } from 'react';
 
-const EditBlogForm = () => {
+const EditBlogForm = ({updateBlog}) => {
+
+    const blog = useLoaderData();
+    const navigate = useNavigate();
+
+    const [stateBlog, setStateBlog] = useState({
+        name : blog.name, 
+        dateOfCreation : blog.dateOfCreation,
+        timeOfCreation : blog.timeOfCreation,
+        userId : blog.userId
+    });
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const newName = event.target[0].value;
+        console.log(newName);
+        const blogCopy = {...blog};
+        blogCopy["name"] = newName;
+        setStateBlog(blogCopy);
+
+        updateBlog(stateBlog);
+        navigate("/1/my_blogs");
+    }
+
     return (
-        <p>Edit Blog Here:</p>
+        <>
+            <p>Edit Blog Here:</p>
+            <form onSubmit={handleFormSubmit}>
+            <label htmlFor="name"> New Blog Name:</label>
+                <input 
+                type= "text" 
+                id="newBlogName" 
+                name='name' ></input>
+                <input type="submit" value='Edit Blog'/>
+            </form>
+        </>
     );
 }
  

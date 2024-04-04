6c453d313e513aa7ccb3621a5ab6551d89b8e350
Sahil Patel <sahilpatel1906@gmail.com>
2024-04-04 05:33:40 -0500

    created structure of login form

--- src/Components/UserLoginForm.jsx
+++ src/Components/UserLoginForm.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
 const UserLoginForm = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <p> Login Goes Here</p>
+        <>
+            <p> Login Goes Here</p>
+            <form>
+                <label htmlFor="username-field">Username: </label>
+                <input 
+                    value={userName} 
+                    name="username"
+                    placeholder="Enter username"
+                    id="username-field"
+                    required
+                    onChange={(event)=> {setUserName(event.target.value)}}
+                />
+                <label htmlFor="password-field">Password: </label>
+                <input 
+                    value={password} 
+                    name="password"
+                    placeholder="Enter password"
+                    id="password-field"
+                    required
+                    onChange={(event)=> {setPassword(event.target.value)}}
+                />
+                <input 
+                    type="Submit"
+                    value="Submit"
+                />
+            </form>
+        </>
     );
 }
  

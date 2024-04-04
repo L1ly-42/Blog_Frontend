ee052d847bb032a0f0c63a01c3847bb64634b318
Hannah Riley <hannah.847@btinternet.com>
2024-04-04 10:22:36 +0100

    align posts in a column:

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -5,20 +5,20 @@ const Post = ({post}) => {
         return   <li>{JSON.stringify(comment.text)}</li>
     }))
     return ( 
-        <>
-        <h3>{post.title}</h3>
-        {post.mediaURL ? <img src={post.mediaURL} alt="post picture"/> : <></>}
-        <p>{post.content}</p>
-        <p>Likes: {post.numberOfLikes}</p>
-        <div className='commentsList'>
-            <h4 id='commentsTitle'>Comments:</h4>
-            <ul className='comments'>   
-                {mappedComments}
-            </ul>
+        <div>
+            <h3>{post.title}</h3>
+            {post.mediaURL ? <img src={post.mediaURL} alt="post picture"/> : <></>}
+            <p>{post.content}</p>
+            <p>Likes: {post.numberOfLikes}</p>
+            <div className='commentsList'>
+                <h4 id='commentsTitle'>Comments:</h4>
+                <ul className='comments'>   
+                    {mappedComments}
+                </ul>
+            </div>
+            <button>Edit Post</button>
+            <button>Delete Post</button>
         </div>
-        <button>Edit Post</button>
-        <button>Delete Post</button>
-        </>
      );
 }
  
